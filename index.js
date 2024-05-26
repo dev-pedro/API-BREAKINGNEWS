338a801b61093e14353385f053d0fa4029079e28
Pedro Henrique <dev.pedro.rjas@gmail.com>
2024-05-26 08:45:26 -0300

    refactor. added the software architecture

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const express = require('express')
+const express = require("express")
+const userRoute = require("./src/routes/user.route")
 const app = express()
 
-app.get('/', (req, res) => {
+app.use("/soma", userRoute)
+
+// Rota
+// Method HTTP - CRUD(REATE, READ, UPDATE, DELETE)
+// GET - Pega uma Info
+// POST - Cria uma info
+// PUT - Atera toda a info
+// PATH - Altera parte da info
+// DELETE - apaga uma info
+// Name - Um identificador da rota
+// Function (callback) - Responsável por executar algum comando
+/* app.get('/', (req, res) => {
   res.send('Hello World')
-})
+}) */
 
 app.listen(3000)
